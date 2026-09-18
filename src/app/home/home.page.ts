4b3c32cf28c5db76ade293a219761eae24e5da89
Edin Licina <edin.licina.2@gmail.com>
2026-09-18 14:31:40 +0200

    display movies in web and android emulator

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,65 @@
-import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular';
+import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
+
+import { CommonModule } from '@angular/common';
+
+import {
+  IonHeader,
+  IonToolbar,
+  IonTitle,
+  IonContent,
+  IonList,
+  IonItem,
+  IonLabel,
+  IonBadge,
+  IonSkeletonText
+} from '@ionic/angular';
+
+import { MovieService } from '../services/movie.service';
+import { Movie } from '../models/movie.model';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: [
+    CommonModule,
+    IonHeader,
+    IonToolbar,
+    IonTitle,
+    IonContent,
+    IonList,
+    IonItem,
+    IonLabel,
+    IonBadge,
+    IonSkeletonText
+  ],
 })
-export class HomePage {
-  constructor() {}
+export class HomePage implements OnInit {
+
+  movies: Movie[] = [];
+  loading = true;
+
+  constructor(
+    private movieService: MovieService,
+    private changeDetectorRef: ChangeDetectorRef
+  ) {}
+
+  ngOnInit(): void {
+    this.movieService.getPopularMovies().subscribe({
+      next: (movies) => {
+        console.log('Movies received:', movies);
+
+        this.movies = movies;
+        this.loading = false;
+
+        this.changeDetectorRef.detectChanges();
+      },
+      error: (error) => {
+        console.error('Error fetching popular movies:', error);
+        this.loading = false;
+
+        this.changeDetectorRef.detectChanges();
+      }
+    });
+  }
 }
